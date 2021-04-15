1629ca5d8c945a0e161a4ed823769a81ce27a62d
Muhammad Sholehhudin <msholehhudin.ms@gmail.com>
2021-04-15 15:18:59 +0700

    Templating E-Commerce Laravel Selesai

---
+++ resources/views/layouts/partials/topbanner.blade.php
@@ -0,0 +1,17 @@
+<div class="container">
+    <div class="header_section_top">
+       <div class="row">
+          <div class="col-sm-12">
+             <div class="custom_menu">
+                <ul>
+                   <li><a href="#">Best Sellers</a></li>
+                   <li><a href="#">Gift Ideas</a></li>
+                   <li><a href="#">New Releases</a></li>
+                   <li><a href="#">Today's Deals</a></li>
+                   <li><a href="#">Customer Service</a></li>
+                </ul>
+             </div>
+          </div>
+       </div>
+    </div>
+ </div>
